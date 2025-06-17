10915453291000c7d613de11f19319433a6ae4d4
Figgy <140565846+Figgys@users.noreply.github.com>
2025-06-17 17:39:40 +0100

    Create grabInfo.py

---
+++ grabInfo.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from cacheTileInfo import pullFromCache
+
+responseTowns = requests.get('https://api.lokamc.com/towns/search/findDeleted')
+
+deletedTowns = {"seenTowns": []}
+currentNum = 0
+
+def appendToList() : 
+    for town in responseTowns.json()['_embedded']['towns'] :
+
+        name = town['name']
+        world = town["world"]
+        tileNum = town["territoryNum"]
+        list = pullFromCache(world, tileNum)
+        biome,x,y,z = list.split(",")
+
+        global currentNum
+        currentNum += 1 
+        print(name, world, tileNum, biome, x, y, z, currentNum) #Anything with current num isnt needed but i like it
+
+        newTownJsonFormat = {
+            'name': name, 
+            "location": {
+                "world" : world,
+                "tileNumber": tileNum,
+                "biome": biome,
+                "x": x,
+                "y": y,
+                "z": z
+                }
+            }
+    
+        deletedTowns['seenTowns'].append(newTownJsonFormat)
+
+
+appendToList()
+
+with open('seenTowns.json', 'w') as file:
+    json.dump(deletedTowns, file, indent=4)
+    print("Now in file")
